8f979ef5b4c068277d8578a8f594f16e082e2bfd
JD Bothma <jbothma@gmail.com>
2017-11-14 10:57:16 +0200

    commit before moving to views

--- generate_data.py
+++ generate_data.py
@@ -2,6 +2,7 @@ from ckanapi import RemoteCKAN
 from collections import defaultdict
 import yaml
 import os
+from slugify import slugify
 
 ckan = RemoteCKAN('https://treasurydata.openup.org.za')
 
@@ -33,13 +34,27 @@ for year_govt_group_id in ckan.action.group_list():
     year = extras_get(year_govt_group['extras'], 'Financial Year')
     geographic_region = extras_get(year_govt_group['extras'], 'Geographic Region')
 
+    department_list_items = []
     departments = []
 
     for year_govt_pkg in year_govt_pkgs:
         print "  %s %s" % (year_govt_pkg['id'], year_govt_pkg['name'])
+        department_name = extras_get(year_govt_pkg['extras'], 'Department Name')
+        vote_number = int(extras_get(year_govt_pkg['extras'], 'Vote Number'))
+        department_list_items.append({
+            'name': department_name,
+            'vote_number': vote_number,
+        })
         departments.append({
-            'name': extras_get(year_govt_pkg['extras'], 'Department Name'),
-            'vote_number': int(extras_get(year_govt_pkg['extras'], 'Vote Number')),
+            'name': department_name,
+            'slug': slugify(department_name),
+            'sphere': 'provincial',
+            'geographic_region': {
+                'name': geographic_region,
+                'slug': slugify(geographic_region),
+            },
+            'vote_number': vote_number,
+            'financial_year': year,
         })
 
     if 'department_list' not in years[year]['provincial']:
@@ -49,6 +64,9 @@ for year_govt_group_id in ckan.action.group_list():
         'departments': sorted(departments, key=lambda d: d['vote_number']),
     })
 
+    years[year]['provincial'][geographic_region] = departments
+
+    break
 
 for year in years.keys():
     years[year]['provincial']['department_list'] \
@@ -68,3 +86,21 @@ for year in years.keys():
         os.makedirs(directory)
     with open(os.path.join(directory, "departments.html"), "wb") as outfile:
         outfile.write("---\nfinancial_year: %s\nlayout: department_list\n---" % year)
+
+    for geographic_region in years[year]['provincial'].keys():
+        for department in years[year]['provincial'][geographic_region]:
+            print department
+            # department pages
+            geographic_region_slug = department['geographic_region']['slug']
+            directory = "%s/provincial/%s/departments" % (year, geographic_region_slug)
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            with open(os.path.join(directory, "%s.html" % department['slug']), "wb") as outfile:
+                outfile.write(
+                    "---\n\
+                    financial_year: %s\n\
+                    sphere: provincial\n\
+                    geographic_region_slug: %s\n\
+                    department_slug: %s\n\
+                    layout: department\n\
+                    ---" % (year, geographic_region_slug, department['slug']))
