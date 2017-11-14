f8f8440a72b3803baf5e6db0a9c46010a85f7e45
JD Bothma <jbothma@gmail.com>
2017-11-14 14:21:46 +0200

    MVC working for dept list page

--- generate/views.py
+++ generate/views.py
@@ -11,10 +11,12 @@ class DepartmentList():
                     'name': department.name,
                     'vote_number': department.vote_number,
                 })
+            departments = sorted(departments, key=lambda d: d['vote_number'])
             self.governments.append({
                 'name': government.name,
                 'departments': departments,
             })
+            self.governments = sorted(self.governments, key=lambda g: g['name'])
 
     def yaml(self, *args, **kwargs):
-        return yaml.safe_dump(*([self.governments] + args), **kwargs)
+        return yaml.safe_dump(*([self.governments] + list(args)), **kwargs)
