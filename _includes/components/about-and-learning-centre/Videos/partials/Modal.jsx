0ca52b70764d6313e51373f258ff61dbab26cc75
Schalk Venter <venterschalk@gmail.com>
2018-02-06 16:18:49 +0200

    Adds remaining style changes and branding

--- _includes/components/about-and-learning-centre/Videos/partials/Modal.jsx
+++ _includes/components/about-and-learning-centre/Videos/partials/Modal.jsx
@@ -17,25 +17,27 @@ export default function Modal(props) {
   return (
     <div className="Videos-modalWrap">
       <div className="Videos-modal">
-        <div className="Videos-modalClose" onClick={closeModal} >
-          <CloseIcon />
-        </div>
-        <div className="Videos-modalTitle">{title}</div>
-        <div className="Videos-embed">
-          <div className="Videos-loading" />
-          <iframe className="Videos-iframe" width="560" height="315" src={`https://www.youtube.com/embed/${open.language}?rel=0&amp;amp;showinfo=0`} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen" />
-        </div>
-        <span className="Videos-label">
-          Change language:
-        </span>
-        <div className="Videos-selectWrap">
-          <PseudoSelect
-            name="language"
-            open={open.select}
-            items={selectOptions}
-            property={open.language}
-            changeAction={value => setLanguage(value)}
-          />
+        <div className="Videos-modalBox">
+          <div className="Videos-modalClose" onClick={closeModal} >
+            <CloseIcon />
+          </div>
+          <div className="Videos-modalTitle">{title}</div>
+          <div className="Videos-embed">
+            <div className="Videos-loading" />
+            <iframe className="Videos-iframe" width="560" height="315" src={`https://www.youtube.com/embed/${open.language}?rel=0&amp;amp;showinfo=0`} frameborder="0" allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen" />
+          </div>
+          <span className="Videos-label">
+            Change language:
+          </span>
+          <div className="Videos-selectWrap">
+            <PseudoSelect
+              name="language"
+              open={open.select}
+              items={selectOptions}
+              property={open.language}
+              changeAction={value => setLanguage(value)}
+            />
+          </div>
         </div>
       </div>
     </div>
