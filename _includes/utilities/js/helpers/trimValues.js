668db406be8d1d759b5ad4e52a5f381f8da82f8d
Schalk Venter <venterschalk@gmail.com>
2018-03-13 10:56:59 +0200

    Regenerates assets

--- _includes/utilities/js/helpers/trimValues.js
+++ _includes/utilities/js/helpers/trimValues.js
@@ -2,10 +2,10 @@ export default function trimValues(value, abbreviated) {
   const million = abbreviated ? 'm' : 'million';
   const billion = abbreviated ? 'bn' : 'billion';
 
-  if (value > 1000000) {
-    return `${Math.round(value / 1000000)} ${million}`;
-  } else if (value > 1000000000) {
+  if (value > 1000000000) {
     return `${Math.round(value / 1000000000)} ${billion}`;
+  } else if (value > 1000000) {
+    return `${Math.round(value / 1000000)} ${million}`;
   }
 
   return Math.round(value);
