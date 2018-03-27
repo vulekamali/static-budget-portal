63015a8b87b1882ed51359890077bd61434b5aff
Schalk Venter <venterschalk@gmail.com>
2018-03-27 17:21:31 +0200

    Conditionally removes decimals if ends in `.0`

--- _includes/utilities/js/helpers/trimValues.js
+++ _includes/utilities/js/helpers/trimValues.js
@@ -3,10 +3,10 @@ export default function trimValues(value, abbreviated) {
   const billion = abbreviated ? 'bn' : 'billion';
 
   if (value > 1000000000) {
-    return `${(value / 1000000000).toFixed(1)} ${billion}`;
+    return `${(value / 1000000000).toFixed(1).replace(/\.0$/, '')} ${billion}`;
   } else if (value > 1000000) {
-    return `${(value / 1000000).toFixed(1)} ${million}`;
+    return `${(value / 1000000).toFixed(1).replace(/\.0$/, '')} ${million}`;
   }
 
-  return value.toFixed(1);
+  return value.toFixed(1).replace(/\.0$/, '');
 }
