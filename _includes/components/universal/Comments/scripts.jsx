eaabc5462bd67d2bf8d1d7649b5c13aa417d29ea
Schalk Venter <venterschalk@gmail.com>
2018-04-23 17:53:46 +0200

    Adds addition asset whitelisting

---
+++ _includes/components/universal/Comments/scripts.jsx
@@ -0,0 +1,16 @@
+
+
+
+
+<script type="text/javascript">
+  DiscourseEmbed = {
+    discourseUrl: 'https://discussions.vulekamali.gov.za/',
+    discourseEmbedUrl: ''
+  };
+
+  (function() {
+    var d = document.createElement('script'); d.type = 'text/javascript'; d.async = true;
+    d.src = DiscourseEmbed.discourseUrl + 'javascripts/embed.js';
+    (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(d);
+  })();
+</script>
