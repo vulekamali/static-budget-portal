d2dee853cc43817e3af7ace42652e8264fb26ead
Schalk Venter <venterschalk@gmail.com>
2018-07-30 11:33:23 +0200

    Changes event arguments sent to GA

--- _includes/components/universal/VideoEmbed/index.jsx
+++ _includes/components/universal/VideoEmbed/index.jsx
@@ -1,4 +1,4 @@
-import { event } from 'react-ga';
+import { ga } from 'react-ga';
 import { h, Component } from 'preact';
 
 
@@ -48,7 +48,7 @@ export default class VideoEmbed extends Component {
   componentDidMount() {
     const { title } = this.props;
     const { selected } = this.state;
-    event('send', 'event', 'video', 'created', title, selected);
+    ga('send', 'event', 'video viewed', title, selected);
   }
 
   componentDidUpdate(prevProps, prevState) {
@@ -56,7 +56,7 @@ export default class VideoEmbed extends Component {
     const { selected } = this.state;
 
     if (prevState.selected !== selected) {
-      event('send', 'event', 'video', 'changed', title, selected);
+      ga('send', 'event', 'video viewed', title, selected);
     }
   }
 
