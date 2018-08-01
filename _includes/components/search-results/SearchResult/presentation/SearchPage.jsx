13c0738a711792896c1860ddc69c62d58ba1006d
Schalk Venter <venterschalk@gmail.com>
2018-08-01 17:30:26 +0200

    Adds documentation

--- _includes/components/search-results/SearchResult/presentation/SearchPage.jsx
+++ _includes/components/search-results/SearchResult/presentation/SearchPage.jsx
@@ -1,15 +1,34 @@
 import { h } from 'preact';
+import PropTypes from 'prop-types';
 import TabSelection from './TabSelection.jsx';
 import LandingLayout from './LandingLayout.jsx';
 import FacetLayout from './FacetLayout.jsx';
 import tabOptions from './../data/tabOptions.json';
 
 
-function calcContent(props) {
+/**
+ *
+ * @param props - Preact props object.
+ * @param props.error - If true shows an error message instead of actual content.
+ * @param props.loading - If true shows a loading spinner instead of actual content.
+ * @param props.updateTab - Callback function that changes tab, sets {@link props.loading} to true
+ * and sends HTTP request for data that will be needed on new tab.
+ * @param props.addPage - Callback that just adds 5 new results to a facet tab.
+ * @param props.tab - Determines what the current tab is, and therefore the layout to return.
+ * @param props.year - Inserted into links and titles that are year specific.
+ * @param props.response - The normalised response object passed to the presentation layer. Usually
+ * contains a 'count' propery inidicating the total amount of search results returned and 'items'
+ * that contains one or more lists of actual returned search results.
+ * @param props.page - A value that is multiplied by 5 in order to determine how many results to
+ * show on a facet page. Always resets to 1 when the tab is changed.
+ */
+function CalcContent(props) {
   const { error, loading } = props;
   const { updateTab, addPage } = props;
   const { tab, year, response, page } = props;
   const { count, items } = response || {};
+
+  console.log(response);
 
   if (error) {
     return (
@@ -47,13 +66,61 @@ function calcContent(props) {
 }
 
 
+/**
+ * Initialised the basic layout, with the title of the search term at the top of the page, and the
+ * menu bar that allows you to switch between tabs. However loads another component that dynamically
+ * determines what should be displayed in the content area.
+ *
+ * @param props - The Preact props object.
+ * @param props.tab - The current displayed tab.
+ * @param props.updateTab - A callback function enables the loading display, changes current tab and
+ * fires new HTTP requests for the data needed for the new tab.
+ * @param props.phrase - The search term as passed from the URL parameter.
+ */
 export default function SearchPage(props) {
   const { tab, updateTab, phrase } = props;
   return (
     <div className="SearchResult">
       <div className="Page-title u-textAlignCenter">Search results for &quot;{phrase}&quot;</div>
       <TabSelection {...{ tab, updateTab, tabOptions }} />
-      {calcContent(props)}
+      <CalcContent {...props} />
     </div>
   );
 }
+
+
+const responseSchema = PropTypes.shape({
+  count: PropTypes.num.isRequired,
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      count: PropTypes.num.isRequired,
+      title: PropTypes.string.isRequired,
+      url: PropTypes.string.isRequired,
+      contributor: PropTypes.string.isRequired,
+      snippet: PropTypes.string,
+      source: {
+        url: PropTypes.string,
+        text: PropTypes.string,
+      },
+    }),
+  ),
+});
+
+SearchPage.propTypes = {
+  tab: PropTypes.oneOf(['all', 'cso', 'national', 'provincial']).isRequired,
+  updateTab: PropTypes.func.isRequired,
+  phrase: PropTypes.string.isRequired,
+  error: PropTypes.bool,
+  loading: PropTypes.bool,
+  addPage: PropTypes.func,
+  year: PropTypes.string.isRequired,
+  page: PropTypes.number.isRequired,
+};
+
+
+SearchPage.defaultProps = {
+  error: false,
+  loading: false,
+  addPage: null,
+  response: {},
+};
