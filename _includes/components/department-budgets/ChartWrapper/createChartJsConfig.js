11f633e6a9ddd30f2fd313eff4987472f014f437
Schalk Venter <venterschalk@gmail.com>
2018-10-04 17:03:57 +0200

    Creates ChartWrapper components

---
+++ _includes/components/department-budgets/ChartWrapper/createChartJsConfig.js
@@ -0,0 +1,147 @@
+import Chart from 'chart.js';
+
+import trimValues from './../../../utilities/js/helpers/trimValues';
+
+
+const calcLabelPosition = (height, x, y, maxWidth) => {
+  if (x > maxWidth / 2) {
+    return {
+      textX: x - (height / 3),
+      textY: y - 7,
+      align: 'right',
+      color: '#2d520d',
+      space: x - (((height / 3) * 2) + 20),
+    };
+  }
+
+  return {
+    textX: x + (height / 3),
+    textY: y - 7,
+    align: 'left',
+    color: '#2d520d',
+    space: maxWidth - (x + (((height / 3) * 2) + 20)),
+  };
+};
+
+
+const calcLabelTruncate = (target, space, label) => {
+  let truncatedLabel = label;
+
+  if (target.measureText(truncatedLabel).width < space) {
+    return truncatedLabel;
+  }
+
+  for (let characters = label.length; characters >= 0; characters--) {
+    truncatedLabel = truncatedLabel.substring(0, characters);
+    if (target.measureText(truncatedLabel).width < space) {
+      break;
+    }
+  }
+
+  return `${truncatedLabel}...`;
+};
+
+
+const createModifyLabel = (target, fontString) => ({ label, height, x, y, maxWidth }) => {
+  const { textX, textY, align, color, space } = calcLabelPosition(height, x, y, maxWidth);
+  const fontFallbacks = 'Source Sans, Arial, sans-serif';
+
+  const regexArray = label.match(/(^heading:\s)(.+)/im)
+  const isHeading = /(^heading:\s)(.+)/im.test(label);
+  const labelAfterHeadingCheck = isHeading ? regexArray[2] : label;
+  const fontStyle = isHeading ? fontString(14, 'bold', fontFallbacks) : fontString(11, 'normal', fontFallbacks);
+
+  const truncatedLabel = calcLabelTruncate(target, space, labelAfterHeadingCheck);
+
+  target.font = fontStyle;
+  target.textBaseline = 'top';
+  target.fillStyle = isHeading ? 'grey' : color;
+  target.textAlign = align;
+  target.fillText(truncatedLabel, isHeading ? 0 : textX, textY);
+};
+
+
+const dnyamicLabelPlugin = ({ chart }) => {     
+  const barInfo = chart.getDatasetMeta(0).data;
+  const modifyLabel = createModifyLabel(chart.ctx, Chart.helpers.fontString);
+
+  barInfo.forEach((bar) => {
+    const { _xScale, _model } = bar;
+
+    const { maxWidth } = _xScale;
+    const { x, y, label, height } = _model;
+    modifyLabel({ label, height, x, y, maxWidth });
+  });
+};
+
+
+function createChartJsConfig({ labels, values }) {
+  return {
+    type: 'horizontalBar',
+    data: {
+      labels,
+      datasets: [
+        {
+          data: values,
+          backgroundColor: '#73b23e',
+        },
+      ],
+    },
+    options: {
+      maintainAspectRatio: false,
+      tooltips: {
+        intersect: false,
+        custom: (tooltip) => {
+          if (!tooltip) {
+            return;
+          }
+          tooltip.displayColors = false;
+        },
+        callbacks: {
+          label: (item, dataObject) => {
+            const { index } = item;
+            const { data } = dataObject.datasets[0];
+            return `R${data[index]}`;
+          },
+        },
+      },
+      animation: {
+        duration: 0,
+      },
+      legend: {
+        display: false,
+      },
+      scales: {
+        yAxes: [{
+          display: false,
+          gridLines: {
+            display: false,
+          },
+          ticks: {
+            display: false,
+          },
+          barPercentage: 0.8,
+          categoryPercentage: 1.0,
+        }],
+        xAxes: [{
+          ticks: {
+            beginAtZero: true,
+            maxRotation: 0,
+            callback: value => trimValues(value),
+          },
+          gridLines: {
+            display: false,
+          },
+        }],
+      },
+    },
+    plugins: [
+      {
+        afterDatasetsDraw: dnyamicLabelPlugin,
+      },
+    ],
+  };
+}
+
+
+export default createChartJsConfig;
---
+++ _includes/components/department-budgets/ChartWrapper/index.jsx
@@ -0,0 +1,55 @@
+import { h, Component } from 'preact';
+import { pick } from 'lodash';
+import Chart from 'chart.js';
+
+import flattenNesting from './flattenNesting.js';
+import createChartJsConfig from './createChartJsConfig.js';
+
+
+export default class ChartWrapper extends Component {
+  constructor(...props) {
+    super(...props);
+
+    this.state = {
+      node: null,
+    };
+
+    this.events = {
+      renderChart: this.renderChart.bind(this),
+    };
+  }
+
+  renderChart(node) {
+    const { node: existingNode } = this.state;
+    const { items } = this.props;
+
+    if (!existingNode) {
+      return this.setState({ node });
+    }
+
+    const chartJsData = flattenNesting(items);
+    const config = createChartJsConfig(chartJsData);
+    const chartInstance = new Chart(existingNode, config);
+
+    return chartInstance;
+  }
+
+  render() {
+    const state = pick(this.state, ['node']);
+    const props = pick(this.props, ['items']);
+    const events = pick(this.events, ['renderChart']);
+    const passedProps = { ...state, ...props, ...events };
+    return <Markup {...passedProps} />;
+  }
+}
+
+
+function Markup({ renderChart, items }) {
+  const { labels } = flattenNesting(items);
+
+  return (
+    <div>
+      <canvas ref={ref => renderChart(ref)} height={labels.length * 30} />
+    </div>
+  );
+}
