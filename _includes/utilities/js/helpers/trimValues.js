0ef161bd9ed5ecb1b4da70b7bf2ad7fad180220d
Schalk Venter <venterschalk@gmail.com>
2018-11-08 14:54:06 +0200

    Adds negative calculation to trimValues service

--- _includes/utilities/js/helpers/trimValues.js
+++ _includes/utilities/js/helpers/trimValues.js
@@ -3,11 +3,26 @@ export default function trimValues(rawValue, abbreviated) {
   const million = abbreviated ? 'm' : 'million';
   const billion = abbreviated ? 'bn' : 'billion';
 
-  if (value > 1000000000) {
+  if (value < 1000000 && value > -1000000) {
+    return value.toFixed(1).replace(/\.0$/, '');
+  }
+
+  if (value >= 1000000000) {
     return `${(value / 1000000000).toFixed(1).replace(/\.0$/, '')} ${billion}`;
-  } else if (value > 1000000) {
+  }
+
+  if (value >= 1000000) {
     return `${(value / 1000000).toFixed(1).replace(/\.0$/, '')} ${million}`;
   }
 
-  return value.toFixed(1).replace(/\.0$/, '');
+  if (value <= 1000000000) {
+    return `${(value / 1000000000).toFixed(1).replace(/\.0$/, '')} ${billion}`;
+  }
+
+  if (value <= 1000000) {
+    return `${(value / 1000000).toFixed(1).replace(/\.0$/, '')} ${million}`;
+  }
+
+
+  return null;
 }
