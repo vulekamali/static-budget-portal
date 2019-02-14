5286e81415d228671edd3513c8f297ca887b0956
Schalk Venter <venterschalk@gmail.com>
2019-02-14 15:28:22 +0200

    Configures Jekyll to receive Webapp script

---
+++ packages/webapp/src/connections/homepage-hero.js
@@ -0,0 +1,34 @@
+import { createElement } from 'react';
+import { render } from 'react-dom';
+import Homepage from '../views/Homepage';
+
+
+const props = {
+  image: 'parliament',
+  heading: 'New budget data will be available soon',
+  subheading: 'The 2019 budget speech',
+  buttons: {
+    primary: {
+      text: 'How to watch the speech',
+      link: '#',
+    },
+    secondary: {
+      text: 'Get notified when data is live',
+      link: '#',
+    },
+  },
+  notice: 'The 2019/20 budget will be live on Vulekamali by 22 February 2019.',
+}
+
+
+const node = document.querySelector('[data-webapp="homepage-hero"]')
+
+
+const connection = () => {
+  if (node) {
+    render(Homepage, node);
+  }
+};
+
+
+export default connection();
