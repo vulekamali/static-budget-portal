7b5ae994de5f08f87a306b506e288f1462a8d37c
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-16 20:44:21 +0200

    complete styling ready for review

--- packages/webapp/src/views/Infrastructure/Preview.jsx
+++ packages/webapp/src/views/Infrastructure/Preview.jsx
@@ -6,7 +6,8 @@ import LinearProgress from '@material-ui/core/LinearProgress';
 import NationalMap from '../../components/NationalMap';
 import { Typography } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
-// import { darken } from 'polished';
+import ForwardArrow from '@material-ui/icons/ArrowForward';
+import { darken } from 'polished';
 
 
 const AnimationWrapper = posed.div({
@@ -21,15 +22,26 @@ const AnimationWrapper = posed.div({
 });
 
 const Wrapper = styled.div`
-  ${'' /* display: flex; */}
+  @media screen and (min-width: 450px) {
+    display: flex;
+  }
 `;
 
-const ListGroup = styled.div`
+const DataGroup = styled.div`
   max-width: 272px;
   margin: 0 auto;
   font-family: Lato;
-`;
-const List = styled.div`
+  padding-top: 16px;
+  padding-bottom: 16px;
+
+  @media screen and (min-width: 450px) {
+    max-width: 419px;
+    padding-left: 20px;
+  }
+
+  @media screen and (min-width: 850px) {
+    padding-left: none;
+  }
 `;
 
 const SubHeading = styled.div`
@@ -40,6 +52,14 @@ const SubHeading = styled.div`
   text-align: center;
   letter-spacing: 3px;
   text-transform: Uppercase;
+  padding-bottom: 14px;
+
+  @media screen and (min-width: 450px) {
+    font-weight: 400;
+    line-height: normal;
+    font-size: 14px;
+    text-align: left;
+  }
 `;
 
 const Heading = styled.div`
@@ -49,6 +69,13 @@ const Heading = styled.div`
   text-align: center;
   text-transform: Capitalize;
   max-width: 272px;
+  padding-bottom: 24px;
+
+  @media screen and (min-width: 450px) {
+    font-size: 32px;
+    text-align: left;
+    max-width: 419px;
+  }
 `;
 
 const Stage = styled.div`
@@ -59,17 +86,32 @@ const Stage = styled.div`
   letter-spacing: 0.5px
   text-transform: Uppercase;
   color: rgba(0, 0, 0, 0.5);
+  padding-bottom: 14px;
+
+  @media screen and (min-width: 450px) {
+    line-height: normal;
+    text-align: left;
+  }
+`;
+
+const ProgressBarContainer = styled.div`
+  padding-bottom: 26px;
 `;
 
 const BudgetGroup = styled.div`
   display: flex;
   justify-content: space-between;
+  padding-bottom: 30px;
 `;
 
 const BudgetCashflow = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+
+  @media screen and (min-width: 450px) {
+    align-items: flex-start;
+  }
 `;
 
 const CashflowTitle = styled.div`
@@ -80,6 +122,11 @@ const CashflowTitle = styled.div`
   letter-spacing: 0.5px
   text-transform: Uppercase;
   color: #696969;
+  padding-bottom: 10px;
+
+  @media screen and (min-width: 450px) {
+    text-align: left;
+  }
 `;
 
 const Estimation = styled.div`
@@ -87,6 +134,11 @@ const Estimation = styled.div`
   line-height: normal;
   font-size: 18px;
   text-align: center;
+
+  @media screen and (min-width: 450px) {
+    font-size: 24px;
+    text-align: left;
+  }
 `;
 
 const Text = styled(Typography)`
@@ -95,9 +147,66 @@ const Text = styled(Typography)`
     line-height: 30px;
     font-size: 14px;
     text-align: center;
+    padding-bottom: 24px;
+
+    @media screen and (min-width: 450px) {
+      line-height: 23px;
+      font-size: 16px;
+      text-align: left;
+      padding-bottom: 32px;
     }
+  }
 `;
 
+const StyledButton = styled(Button)`
+  && {
+    margin: 0 auto;
+    min-width: 212px;
+    border-radius: 30px;
+    padding: 16px 20px;
+    color: #fff;
+    background-color: #79B443;
+    font-size: 16px;
+    font-weight: 700;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    text-transform: none;
+
+    &:hover {
+      background: ${darken(0.1, '#79B443')};
+    }
+
+    @media screen and (min-width: 450px) {
+      margin: 0;
+    }
+  }
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+`;
+
+const StyledLinearProgress = styled(LinearProgress)`
+  && {
+    height: 16px;
+    border-radius: 200px;
+    background: #DCDCDC;
+
+    & .barColorPrimary {
+      background: linear-gradient(90deg, rgba(255, 255, 255, 0.8) 0%, #ECA03E 83.33%), #ECA03E;
+      border-right: 2px solid;
+    }
+  }
+`;
+
+const MapWrapper = styled.div`
+  display: none;
+
+  @media screen and (min-width: 650px) {
+     display: block;
+  }
+`;
 
 const createItem = (props) => {
   const {
@@ -114,26 +223,32 @@ const createItem = (props) => {
   return (
     <AnimationWrapper key={id}>
       <Wrapper>
-        <NationalMap active="Eastern Cape" size="small" />
-        <ListGroup>
+        <MapWrapper>
+          <NationalMap active="Eastern Cape" />
+        </MapWrapper>
+        <DataGroup>
           <SubHeading >{subheading}</SubHeading>
           <Heading>{heading}</Heading>
-          <Stage>{stage}</Stage>
-          {calcProgress(stage) && <List><LinearProgress variant="determinate" value={calcProgress(stage)} /></List>}
+          <Stage>Project stage: {stage}</Stage>
+          {calcProgress(stage) && <ProgressBarContainer><StyledLinearProgress classes={{barColorPrimary: 'barColorPrimary', root: 'root'}} variant="determinate" value={calcProgress(stage)} /></ProgressBarContainer>}
           <BudgetGroup>
             <BudgetCashflow>
-              <CashflowTitle>Total budget</CashflowTitle>
+              <CashflowTitle>Total budget:</CashflowTitle>
               <Estimation>{`R${trimValues(totalBudget)}`}</Estimation>
             </BudgetCashflow>
             <BudgetCashflow>
-              <CashflowTitle>3 Years project budget</CashflowTitle>
+              <CashflowTitle>3 Years project budget:</CashflowTitle>
               <Estimation>{`R${trimValues(projectedBudget)}`}</Estimation>
             </BudgetCashflow>
           </BudgetGroup>
           <Text>{description}</Text>
-          <a href={link}></a>
-          <Button></Button>
-        </ListGroup>
+            <StyledLink href={link}>
+              <StyledButton>
+                <span>View in more detail</span>
+                <ForwardArrow />
+              </StyledButton>
+            </StyledLink>
+        </DataGroup>
       </Wrapper>
     </AnimationWrapper>
   )
