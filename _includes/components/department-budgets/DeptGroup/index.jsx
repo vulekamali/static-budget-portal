89bdc1531441a5e99233478e25f430eac7305d2a
JD Bothma <jbothma@gmail.com>
2019-02-19 13:42:47 +0200

    Fix department page to show right year and work without epres.yaml

--- _includes/components/department-budgets/DeptGroup/index.jsx
+++ _includes/components/department-budgets/DeptGroup/index.jsx
@@ -10,7 +10,7 @@ export default function DeptGroup({ map, linksArray, name: title, doubleRow, emp
       <div className="DeptGroup">
         <div className="DeptGroup-wrap">
           <h3 className="DeptGroup-title">{title} Department Budgets</h3>
-          <p className="u-fontStyle u-fontStyle--italic">This data is not yet available. Provincial budgets are tabled after the national budget has been announced. This is because the national budget determines the amount of money each province receives. We expect to be able make provincial budget data available by 29 March 2018.</p>
+          <p className="u-fontStyle u-fontStyle--italic">This data is not yet available. Provincial budgets are tabled after the national budget has been announced. This is because the national budget determines the amount of money each province receives. We expect to be able make provincial budget data available by April {(new Date()).getFullYear()}.</p>
           { epre ? <a target="_blank" className="Button is-inline" href={epre}>Download the full EPRE PDF</a> : null }
         </div>
         <div className="DeptGroup-map">
