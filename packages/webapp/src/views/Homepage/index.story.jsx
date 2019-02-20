21677ff06d2d81479666ac2237179973143b9f60
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-02-20 14:12:24 +0200

    change video to Youtube link

--- packages/webapp/src/views/Homepage/index.story.jsx
+++ packages/webapp/src/views/Homepage/index.story.jsx
@@ -26,7 +26,7 @@ const duringSpeechProps = {
   buttons: {
     primary: {
       text: 'Watch the speech',
-      link: 'http://www.treasury.gov.za/documents/national%20budget/2019/webcast.aspx'
+      link: 'https://www.youtube.com/watch?v=MQNQWBdnC70'
     },
     secondary: {
       text: 'Download budget resources',
