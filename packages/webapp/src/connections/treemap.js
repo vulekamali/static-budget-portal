0a7356b9776d5d4714690cb365f40c27efa894f8
ZeeshaanMaudar <maudarbocus.zeeshaan@gmail.com>
2019-03-18 15:06:13 +0200

    move colorArray into components folder inside of Treemap component

--- packages/webapp/src/connections/treemap.js
+++ packages/webapp/src/connections/treemap.js
@@ -1,6 +1,7 @@
 import { createElement } from 'react';
 import { render } from 'react-dom';
 import TreemapSection from '../components/TreemapSection';
+import { colorArray } from '../components/TreemapSection/Treemap/data/colors'
 
 const node = document.querySelector('[data-webapp="national-departments-treemap"]');
 const dataString = document.querySelector('[data-webapp-departments]').dataset.webappDepartments;
@@ -11,51 +12,6 @@ const originalBudget = amounts.filter(function(amount) {
 });
 const sorted = originalBudget.sort((a, b) => b.amount - a.amount);
 const biggest = sorted.slice(0, 10);
-
-
-const colorArray = [
-  '#FFD54F',
-  '#E57373',
-  '#4DD0E1',
-  '#7986CB',
-  '#BA68C8',
-  '#4DB6AC',
-  '#A1887F',
-  '#64B5F6',
-  '#E57373',
-  '#FF8A65',
-  '#FFD54F',
-  '#4DD0E1',
-  '#F06292',
-  '#7986CB',
-  '#BA68C8',
-  '#4DB6AC',
-  '#9575CD',
-  '#A1887F',
-  '#F06292',
-  '#FFF176',
-  '#FF8A65',
-  '#64B5F6',
-  '#81C784',
-  '#9575CD',
-  '#E57373',
-  '#FFD54F',
-  '#4DD0E1',
-  '#BA68C8',
-  '#A1887F',
-  '#4DB6AC',
-  '#7986CB',
-  '#FFF176',
-  '#4DD0E1',
-  '#BA68C8',
-  '#9575CD',
-  '#FF8A65',
-  '#64B5F6',
-  '#F06292',
-  '#81C784',
-  '#7986CB',
-];
-
 const colored = index => colorArray[index];
 
 const coloured = biggest.map((amount, index) => ({
