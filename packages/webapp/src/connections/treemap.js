6ebd52718db994da1498af5b3bc12a25af6a7577
ZeeshaanMaudar <maudarbocus.zeeshaan@gmail.com>
2019-03-18 14:22:39 +0200

    add additional condition for filter and add colors in json api from client side

--- packages/webapp/src/connections/treemap.js
+++ packages/webapp/src/connections/treemap.js
@@ -4,32 +4,69 @@ import TreemapSection from '../components/TreemapSection';
 
 const node = document.querySelector('[data-webapp="national-departments-treemap"]');
 const dataString = document.querySelector('[data-webapp-departments]').dataset.webappDepartments;
-console.log(dataString);
 const pageData = JSON.parse(dataString);
 const amounts = pageData.expenditure.national;
 const originalBudget = amounts.filter(function(amount) {
-  return amount.budget_phase == "Main appropriation";
+  return amount.budget_phase === "Main appropriation" && amount.financial_year === 2019;
 });
 const sorted = originalBudget.sort((a, b) => b.amount - a.amount);
 const biggest = sorted.slice(0, 10);
 
-console.log(biggest);
 
-function getRandomColor() {
-  var letters = '0123456789ABCDEF';
-  var color = '#';
-  for (var i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
+const colorArray = [
+  '#FFD54F',
+  '#E57373',
+  '#4DD0E1',
+  '#7986CB',
+  '#BA68C8',
+  '#4DB6AC',
+  '#A1887F',
+  '#64B5F6',
+  '#E57373',
+  '#FF8A65',
+  '#FFD54F',
+  '#4DD0E1',
+  '#F06292',
+  '#7986CB',
+  '#BA68C8',
+  '#4DB6AC',
+  '#9575CD',
+  '#A1887F',
+  '#F06292',
+  '#FFF176',
+  '#FF8A65',
+  '#64B5F6',
+  '#81C784',
+  '#9575CD',
+  '#E57373',
+  '#FFD54F',
+  '#4DD0E1',
+  '#BA68C8',
+  '#A1887F',
+  '#4DB6AC',
+  '#7986CB',
+  '#FFF176',
+  '#4DD0E1',
+  '#BA68C8',
+  '#9575CD',
+  '#FF8A65',
+  '#64B5F6',
+  '#F06292',
+  '#81C784',
+  '#7986CB',
+];
 
-const coloured = biggest.map(amount => amount.color = getRandomColor());
+const colored = index => colorArray[index];
+
+const coloured = biggest.map((amount, index) => ({
+  color: colored(index),
+  ...amount
+}));
 
 const connection = () => {
   if (node) {
     return render(
-        createElement(TreemapSection, { departments: biggest, isNationalBudget: true }),
+        createElement(TreemapSection, { departments: coloured, isNationalBudget: true }),
         node
     )
   }
