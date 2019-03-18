163d0e18b1c703926b3a3c0f38c80bb8bf19f451
JD Bothma <jbothma@gmail.com>
2019-03-18 13:03:08 +0200

    Select the top original budget items

--- packages/webapp/src/connections/treemap.js
+++ packages/webapp/src/connections/treemap.js
@@ -8,16 +8,17 @@ console.log(dataString);
 const pageData = JSON.parse(dataString);
 const amounts = pageData.expenditure.national;
 const originalBudget = amounts.filter(function(amount) {
-  return amount.budget_phase == "Main Appropriation";
+  return amount.budget_phase == "Main appropriation";
 });
-
+const sorted = originalBudget.sort((a, b) => b.amount - a.amount);
+const biggest = sorted.slice(0, 10);
 
 console.log(originalBudget);
 
 const connection = () => {
   if (node) {
     return render(
-        createElement(TreemapSection, { departments: amounts, isNationalBudget: true }),
+        createElement(TreemapSection, { departments: biggest, isNationalBudget: true }),
         node
     )
   }
