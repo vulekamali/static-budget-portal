665972f9478df1c2be95c58bcfd6fea0aacecb7e
Jaron Rademeyer <jaronrademeyer@gmail.com>
2019-03-21 18:34:59 +0200

    * Re init treemap

--- packages/webapp/src/components/TreemapSection/index.jsx
+++ packages/webapp/src/components/TreemapSection/index.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import Markup from './Markup';
 import {colorArray} from './Treemap/data/colors'
-import trimValues from '../../helpers/trimValues';
 
 class TreeMapSection extends Component {
     constructor(props) {
@@ -56,10 +55,15 @@ class TreeMapSection extends Component {
                 zoomInButtonState: false,
                 zoomOutButtonState: false,
             });
-            console.log(this.treemapNode);
-            // this.treemapNode.data(splicedData).draw();
+            const parent = document.getElementById("treemap");
+            console.log(parent);
+            while (parent.firstChild) {
+                parent.firstChild.remove();
+            }
+            console.log(parent);
+            this.initTreemap(null)
         }
-        if (spliceIndex+this.zoomStep >= this.fullData.length) {
+        if (spliceIndex + this.zoomStep >= this.fullData.length) {
             this.setState({
                 zoomInButtonState: true
             })
@@ -89,30 +93,6 @@ class TreeMapSection extends Component {
         }
     }
 
-    // initTreemap() {
-    //     return window.d3plus.viz()
-    //         .container("#treemap")
-    //         .data(this.state.departmentData.expenditure.national)
-    //         .type("tree_map")
-    //         .id("name")
-    //         .size("amount")
-    //         .font({"family": "Roboto", size: 15})
-    //         .labels({
-    //             "align": "left",
-    //             "valign": "top",
-    //             "padding": 50,
-    //             "resize": false,
-    //             text: d => `${d.name} - R${trimValues(d.amount)}`
-    //         })
-    //         .color(d => d.color ? d.color : "")
-    //         .legend(false)
-    //         .resize(true)
-    //         .mouse({
-    //             click: this.eventHandler
-    //         })
-    //         .draw();
-    // }
-
     componentDidMount() {
         this.treemapNode = this.initTreemap(this.eventHandler);
         this.fullData = this.props.spendingData['expenditure']['national'];
@@ -121,96 +101,96 @@ class TreeMapSection extends Component {
 
     initTreemap(clickCallback) {
 
-    const parent = window.d3.select('#treemap');
-    const svg = parent.append("svg");
-    const gNode = svg.append("g");
-    const treemapLayout = window.d3.treemap();
+        const parent = window.d3.select('#treemap');
+        const svg = parent.append("svg");
+        const gNode = svg.append("g");
+        const treemapLayout = window.d3.treemap();
 
-    const hierarchy = {
-      "children": this.state.departmentData.expenditure.national,
-    };
-    var rootNode = window.d3.hierarchy(hierarchy);
-    console.log(rootNode);
-    rootNode.sum(function(d) {
-      return d.amount;
-    });
-
-    var paddingAllowance = 2;
-    var nodes = gNode
-        .selectAll("g")
-        .data(rootNode.descendants())
-        .enter()
-        .append("g");
-
-    nodes
-      .append("rect")
-      .style("fill", function(d) {
-        return d.data.color;
-      })
-      .on("click", function(d) {
-          console.log(d);
-        clickCallback(d.data);
-      });
-
-    nodes
-      .append("text")
-      .on("click", function(d) {
-        clickCallback(d.data);
-      });
-
-    const resize = function() {
-      const width = parseFloat(parent.style("width"));
-      const height = parseFloat(parent.style("height"));
-      treemapLayout.size([width, height]);
-      treemapLayout(rootNode);
-      gNode
-        .selectAll("g")
-        .attr("transform", function(d) {
-          return "translate(" + [d.x0, d.y0] + ")";
+        const hierarchy = {
+            "children": this.state.departmentData.expenditure.national,
+        };
+        var rootNode = window.d3.hierarchy(hierarchy);
+        console.log(rootNode);
+        rootNode.sum(function (d) {
+            return d.amount;
         });
-      gNode
-        .selectAll("rect")
-        .attr("width", function(d) {
-          return d.x1 - d.x0;
-        })
-        .attr("height", function(d) {
-          return d.y1 - d.y0;
-        });
-      const nameFontSize = 16;
-      const textPadding = 16;
-      gNode
-        .selectAll("text")
-        .attr("transform", "translate(" + textPadding + ", " + (textPadding + nameFontSize) + ")")
-        .text(function(d) {
-          return (d.y1 - d.y0) < 16 ? "" : d.data.name;
-        })
-        .filter(function(d) {
-          d.tw = this.getComputedTextLength();
-          return (d.x1 - d.x0) < d.tw;
-        })
-        .each(function(d) {
-          // ridiculous routine where we test to see if label is short enough to fit
-          var proposedLabel = d.data.name;
-          var proposedLabelArray = proposedLabel.split("");
-          while (d.tw > (d.x1 - d.x0) && proposedLabelArray.length) {
-            // pull out 3 chars at a time to speed things up (one at a time is too slow)
-            proposedLabelArray.pop();
-            proposedLabelArray.pop();
-            proposedLabelArray.pop();
-            if (proposedLabelArray.length === 0) {
-              proposedLabel = "";
-            } else {
-              proposedLabel = proposedLabelArray.join("") + "..."; // manually truncate with ellipsis
-            }
-            window.d3.select(this).text(proposedLabel);
-            d.tw = this.getComputedTextLength();
-          }
-        });
-    };
-    resize();
-    window.d3.select(window).on("resize", resize);
-    return rootNode;
-  }
+
+        var paddingAllowance = 2;
+        var nodes = gNode
+            .selectAll("g")
+            .data(rootNode.descendants())
+            .enter()
+            .append("g");
+
+        nodes
+            .append("rect")
+            .style("fill", function (d) {
+                return d.data.color;
+            })
+            .on("click", function (d) {
+                console.log(d);
+                clickCallback(d.data);
+            });
+
+        nodes
+            .append("text")
+            .on("click", function (d) {
+                clickCallback(d.data);
+            });
+
+        const resize = function () {
+            const width = parseFloat(parent.style("width"));
+            const height = parseFloat(parent.style("height"));
+            treemapLayout.size([width, height]);
+            treemapLayout(rootNode);
+            gNode
+                .selectAll("g")
+                .attr("transform", function (d) {
+                    return "translate(" + [d.x0, d.y0] + ")";
+                });
+            gNode
+                .selectAll("rect")
+                .attr("width", function (d) {
+                    return d.x1 - d.x0;
+                })
+                .attr("height", function (d) {
+                    return d.y1 - d.y0;
+                });
+            const nameFontSize = 16;
+            const textPadding = 16;
+            gNode
+                .selectAll("text")
+                .attr("transform", "translate(" + textPadding + ", " + (textPadding + nameFontSize) + ")")
+                .text(function (d) {
+                    return (d.y1 - d.y0) < 16 ? "" : d.data.name;
+                })
+                .filter(function (d) {
+                    d.tw = this.getComputedTextLength();
+                    return (d.x1 - d.x0) < d.tw;
+                })
+                .each(function (d) {
+                    // ridiculous routine where we test to see if label is short enough to fit
+                    var proposedLabel = d.data.name;
+                    var proposedLabelArray = proposedLabel.split("");
+                    while (d.tw > (d.x1 - d.x0) && proposedLabelArray.length) {
+                        // pull out 3 chars at a time to speed things up (one at a time is too slow)
+                        proposedLabelArray.pop();
+                        proposedLabelArray.pop();
+                        proposedLabelArray.pop();
+                        if (proposedLabelArray.length === 0) {
+                            proposedLabel = "";
+                        } else {
+                            proposedLabel = proposedLabelArray.join("") + "..."; // manually truncate with ellipsis
+                        }
+                        window.d3.select(this).text(proposedLabel);
+                        d.tw = this.getComputedTextLength();
+                    }
+                });
+        };
+        resize();
+        window.d3.select(window).on("resize", resize);
+        return rootNode;
+    }
 
 
     render() {
