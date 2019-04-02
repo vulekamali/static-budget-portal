4aa7d626d02232a843da23d12a77772842e25c38
Jaron Rademeyer <jaronrademeyer@gmail.com>
2019-04-02 16:25:35 +0200

    * Add YAML data generation code

--- generate/from_dynamic.py
+++ generate/from_dynamic.py
@@ -28,10 +28,22 @@ YEAR_SLUGS = [
     '2016-17',
 ]
 
-TREEMAP_BUDGET_TYPES = [
+BUDGET_TYPES = [
     'original',
     'adjusted',
     'actual'
+]
+
+PROVINCE_SLUGS = [
+    'gauteng',
+    'north-west',
+    'kwazulu-natal',
+    'western-cape',
+    'eastern-cape',
+    'northern-cape',
+    'free-state',
+    'mpumulanga',
+    'limpopo'
 ]
 
 BASIC_PAGE_SLUGS = [
@@ -390,9 +402,11 @@ else:
             project_file.write(GENERATED_YAML_COMMENT)
             project_file.write(r.text)
 
+# Treemaps
+
 SPHERES = ('national', 'provincial')
 for year in YEAR_SLUGS:
-    for budget_phase in TREEMAP_BUDGET_TYPES:
+    for budget_phase in BUDGET_TYPES:
         for sphere in SPHERES:
             listing_url_path = '/{}/{}/{}'.format(year, sphere, budget_phase)
             logger.info(listing_url_path)
@@ -406,3 +420,37 @@ for year in YEAR_SLUGS:
                 ensure_file_dirs(listing_path)
                 with open(listing_path, 'wb') as dataset_list_file:
                     dataset_list_file.write(r.text)
+
+# Department preview pages
+
+GOVERNMENTS = PROVINCE_SLUGS
+for year in YEAR_SLUGS:
+    for budget_phase in BUDGET_TYPES:
+        for sphere in SPHERES:
+            if sphere == 'provincial':
+                for government in GOVERNMENTS:
+                    listing_url_path = '/{}/previews/{}/{}/{}'.format(year, sphere, government, budget_phase)
+                    logger.info(listing_url_path)
+                    listing_url = portal_url + listing_url_path[1:] + '.yaml'
+                    r = http_get(session, listing_url)
+                    if r.status_code == 404:
+                        logger.info("No data for {}".format(listing_url))
+                    else:
+                        r.raise_for_status()
+                        listing_path = '_data%s.yaml' % listing_url_path
+                        ensure_file_dirs(listing_path)
+                        with open(listing_path, 'wb') as dataset_list_file:
+                            dataset_list_file.write(r.text)
+            elif sphere == 'national':
+                listing_url_path = '/{}/previews/{}/south-africa/{}'.format(year, sphere, budget_phase)
+                logger.info(listing_url_path)
+                listing_url = portal_url + listing_url_path[1:] + '.yaml'
+                r = http_get(session, listing_url)
+                if r.status_code == 404:
+                    logger.info("No data for {}".format(listing_url))
+                else:
+                    r.raise_for_status()
+                    listing_path = '_data%s.yaml' % listing_url_path
+                    ensure_file_dirs(listing_path)
+                    with open(listing_path, 'wb') as dataset_list_file:
+                        dataset_list_file.write(r.text)
