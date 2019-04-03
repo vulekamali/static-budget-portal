2c8cb0c29f3900e7f7e8b646eb8bdbd997853643
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-03 15:27:03 +0200

    start building DataLoader

--- packages/webapp/src/adaptors/preview-pages/DataLoader.js
+++ packages/webapp/src/adaptors/preview-pages/DataLoader.js
@@ -1,1 +1,14 @@
 import { Component } from 'react';
+
+class DataLoader extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      data: null
+    }
+  }
+}
+
+export default DataLoader;
