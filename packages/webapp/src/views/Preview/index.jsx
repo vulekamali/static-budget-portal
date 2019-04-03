dd97195d96a1afcfe1632ecf6b59c3d40170144c
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-03 12:51:56 +0200

    Add Description and Footer into View

---
+++ packages/webapp/src/views/Preview/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+import Intro from '../../components/Intro';
+import SectionHeading from '../../components/SectionHeading';
+import BarChart from '../../components/BarChart';
+
+import {
+  TextWrapper,
+  TextContainer,
+  Description,
+  FooterWrapper,
+  FooterContainer,
+  FooterDetails
+} from './styled';
+
+const Preview = ({ resources, items, description }) => (
+  <React.Fragment>
+    <Intro {...resources} />
+    <SectionHeading title='Department information' />
+    <TextWrapper>
+      <TextContainer>
+        <Description>{description}</Description>
+      </TextContainer>
+    </TextWrapper>
+    <SectionHeading title='Department programmes' />
+    <BarChart {...{ items }} />
+    <FooterWrapper>
+      <FooterContainer>
+        <FooterDetails>Budget data from to confirm date</FooterDetails>
+      </FooterContainer>
+    </FooterWrapper>
+  </React.Fragment>
+);
+
+export default Preview;
