ac56639fb9d21238e1fdf370fc287312bd222e20
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-05 11:17:32 +0200

    start fixing CustomizedSelect

--- packages/webapp/src/views/Preview/Heading/CustomizedSelect/index.jsx
+++ packages/webapp/src/views/Preview/Heading/CustomizedSelect/index.jsx
@@ -7,9 +7,9 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Icon from './arrow-drop-down.svg';
 
 const FormControlCtrl = styled(FormControl)`
-  font-weight: bold;
+  font-weight: 700;
   width: 70%;
-  @media screen and (max-width: 675px){
+  @media screen and (max-width: 675px) {
     width: 100%;
   }
 `;
@@ -20,37 +20,39 @@ const SelectPreview = styled(Select)`
   height: 65px;
   margin-top: 29px;
   margin-bottom: 15px;
-  &&{
+
+  && {
     font-size: 24px;
     background: white;
     padding-left: 20px;
-    @media screen and (max-width: 675px){
+
+    @media screen and (max-width: 675px) {
       margin-top: 10px;
       height: 42px;
       font-size: 14px;
       padding-left: 15px;
     }
   }
-  &::before{
-    display:none;
+
+  &::before {
+    display: none;
   }
 `;
 
 
 const ImageIcon = styled.img`
     position: absolute;
-    background: #D7D7D7;
+    background: #d7d7d7;
     bottom: -12px;
-    right: 0px;
+    right: 0;
     height: 7px;
     padding: 29px;
     cursor: pointer;
     pointer-events: none;
     border-top-right-radius: 2px;
     border-bottom-right-radius: 2px;
-    @media screen and (max-width: 675px){
+    @media screen and (max-width: 675px) {
       bottom: -6px;
-      right: 0px;
       height: 4px;
       padding: 19px;
     }
