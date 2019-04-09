e6d4144d03f888cdbffbe731fe4978dda8b5bab7
Schalk Venter <venterschalk@gmail.com>
2019-04-09 16:15:43 +0200

    Adds additional polyfills

--- packages/webapp/src/index.js
+++ packages/webapp/src/index.js
@@ -2,6 +2,9 @@ import 'core-js/fn/object/assign';
 import 'core-js/fn/array/find';
 import 'core-js/fn/promise';
 import 'core-js/fn/weak-set'; 
+import 'core-js/fn/weak-map';
+import 'core-js/fn/set'; 
+import 'core-js/fn/map';
 
 import './adaptors/homepage-hero';
 import './adaptors/infrastructure-pages';
