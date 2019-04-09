d54ffe003ec0492546026895f096818be5b22f01
Schalk Venter <venterschalk@gmail.com>
2019-04-09 16:14:29 +0200

    Adds additional core-js polyfills

--- packages/webapp/src/index.js
+++ packages/webapp/src/index.js
@@ -1,3 +1,8 @@
+import 'core-js/fn/object/assign';
+import 'core-js/fn/array/find';
+import 'core-js/fn/promise';
+import 'core-js/fn/weak-set'; 
+
 import './adaptors/homepage-hero';
 import './adaptors/infrastructure-pages';
 import './adaptors/national-treemap';
