47d6741a5587865471853fdf0f4079a9e856ca24
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-26 12:59:05 +0200

    Add Notices Component to FocusArea

---
+++ packages/webapp/src/views/FocusArea/Notices.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import { Typography } from '@material-ui/core';
+
+const Wrapper = styled.div`
+  background-color: rgba(0, 0, 0, 0.04);
+  ${'' /* background-color: red; */}
+  padding-top: 16px;
+  padding-bottom: 16px;
+`;
+
+const Text = styled(Typography)`
+  && {
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 25px;
+    text-align: center;
+    color: #000;
+  }
+`;
+
+const Notices = ({ items }) => (
+  <Wrapper>
+    <Text>{items}</Text>
+  </Wrapper>
+);
+
+export default Notices;
