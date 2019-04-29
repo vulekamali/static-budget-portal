64cede7493b78772ebe089a5cc5a4ae996dc0635
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-29 17:19:03 +0200

    adjust transform data for focus area

--- packages/webapp/src/adaptors/focus-areas-preview/transformData.js
+++ packages/webapp/src/adaptors/focus-areas-preview/transformData.js
@@ -1,39 +1,84 @@
-const createProgrammeObject = ({ amount, title }) => ({
+const createChildren = ({ amount, name, province, slug, percentage_of_total }) => ({
   amount,
-  name: title
+  name,
+  province,
+  id: slug,
+  percentage: percentage_of_total
 });
+
+const createProvinces = details => {
+  const { province, name, slug, percentage_of_total, amount, url } = details;
+  return {
+    [province]: {
+      name,
+      amount,
+      id: slug,
+      url,
+      percentage: percentage_of_total,
+      children: createChildren(details)
+    }
+  }
+};
+
+// const createProvincialObjects = ({ notices, footnotes, data }) => ({
+//   notices,
+//   footnotes,
+//   provinces: data.map(createProvinces)
+// });
+
+const createNationalDepartments = ({ title, slug, amount }) => ({
+  name: title,
+  amount,
+  url: slug
+});
+
+// const createNationalObjects = ({ notices, footnotes, data }) => ({
+//   notices,
+//   footnotes,
+//   departments: data.map(createNationalDepartments)
+// });
 
 const transformData = (response) => {
   console.log(response)
-  const departmentSchema = response.items.map(department => {
+  const focusSchema = response.items.map(department => {
     const {
-      percentage_changed: percentage,
       total,
       national,
-      description,
       title,
       slug,
-      url,
+      provincial
     } = department;
 
+    const { notices: nationalNotices, footnotes: nationalFootnotes, data: nationalData } = national;
+    const { notices: provincialNotices, footnotes: provincialFootnotes, data: provincialData } = provincial;
+
     return {
-      resources: {
-        consolidated: percentage,
-        value: total
-      },
-      items: national.map(createProgrammeObject),
-      description,
       name: title,
       id: slug,
-      url,
-      total
+      national: {
+        notices: nationalNotices,
+        footnotes: nationalFootnotes,
+        departments: nationalData.map(createNationalDepartments) 
+      },
+      total,
+      provincial: {
+        notices: provincialNotices,
+        footnotes: provincialFootnotes,
+        provinces: provincialData.map(createProvinces)
+      }
     }
   })
 
-  console.log(departmentSchema)
+  console.log(focusSchema)
 
- return departmentSchema;
+ return focusSchema;
 
 };
 
 export default transformData;
+
+
+
+// notices: nationalNotices,
+//         footnotes: nationalFootnotes,
+//         departments: nationalData.map(createNationalDepartments) 
