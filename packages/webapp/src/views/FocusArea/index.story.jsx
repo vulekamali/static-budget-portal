0ce3b430eeead3c5d3b05e13b6b3a1cb320a6ce6
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-29 13:04:58 +0200

    adjust storybook to take different scenarios from different jsons

--- packages/webapp/src/views/FocusArea/index.story.jsx
+++ packages/webapp/src/views/FocusArea/index.story.jsx
@@ -1,108 +1,30 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import FocusAreaTemp from './';
+import allComponents from './storybookData/allComponents';
+import provincesNull from './storybookData/provincesNull';
+import provincesEmpty from './storybookData/provincesEmpty';
+import noEquitableShare from './storybookData/noEquitableShare';
 
-const item = (value) => ({
-  national: [
-    {
-      name: 'Administration',
-      amount: 695320000000,
-      url: null
-    },
-    {
-      name: 'Economic Statistics',
-      amount: 493210000000,
-      url: null
-    },
-    {
-      name: 'Population & Social Statistics',
-      amount: 202300000000,
-      url: null
-    },
-    {
-      name: 'Methodology, Standards & Reasearch',
-      amount: 67400000000,
-      url: null
-    },
-    {
-      name: 'Statistical Support & Informatics',
-      amount: 267100000000,
-      url: null
-    },
-    {
-      name: 'Statistical Collection & Outreach',
-      amount: 608000000000,
-      url: null
-    },
-    {
-      name: 'Survey Operations',
-      amount: 194740000000,
-      url: null
-    }
-  ],
-  // items: null,
-  notices: ['This data will be available upon the release of the 2019-20 Provincial Budgets'],
-  name: 'Something',
-  footnote: [
-    'sourceEquitable: Estimates of National Expenditure 2017-19',
-    'note: Provincial  Share is excluded as it is not part of national departments’ operations expenditure.'
-  ],
-  provincial: [
-    {
-      name: 'Administration',
-      amount: 695320000000,
-      url: null
-    },
-    {
-      name: 'Economic Statistics',
-      amount: 493210000000,
-      url: null
-    },
-    {
-      name: 'Population & Social Statistics',
-      amount: 202300000000,
-      url: null
-    },
-    {
-      name: 'Methodology, Standards & Reasearch',
-      amount: 67400000000,
-      url: null
-    },
-    {
-      name: 'Statistical Support & Informatics',
-      amount: 267100000000,
-      url: null
-    },
-    {
-      name: 'Statistical Collection & Outreach',
-      amount: 608000000000,
-      url: null
-    },
-    {
-      name: 'Survey Operations',
-      amount: 194740000000,
-      url: null
-    }
-  ],
-  // items: null,
-  notices: ['This data will be available upon the release of the 2019-20 Provincial Budgets'],
-  name: 'Something',
-  footnote: [
-    'sourceEquitable: Estimates of National Expenditure 2017-19',
-    'note: Provincial  Share is excluded as it is not part of national departments’ operations expenditure.'
-  ],
-  id: value
-});
-
+//case where every component is showing
+const item = (value) => (
+  allComponents(value)
+);
 
 const items = [1,2,3,4,5].map((value) => {
   return item(value)
 });
 
-
-const initialSelected = {
-  name: 'Provincial Department Contributions',
+const initialSelectedNational = {
+  name: 'National Department Contributions',
   value: 92348259852,
+  url: null,
+  color: 'rgba(0, 0, 0, 0.1)'
+}
+
+const initialSelectedProvincial = {
+  name: 'Provincial Department Contributions',
+  value: 87528439188,
   url: null,
   color: 'rgba(0, 0, 0, 0.1)'
 }
@@ -110,12 +32,80 @@ const initialSelected = {
 const props = {
   items,
   department: 2,
-  initialSelected
+  initialSelectedNational,
+  initialSelectedProvincial
+}
+
+
+//case where initialSelectedProvincial is null
+const propsNoInitialSelectedProvincial = {
+  items,
+  department: 2,
+  initialSelectedNational
+}
+
+
+// case where provinces list is null
+const itemNull = (value) => (
+  provincesNull(value)
+);
+
+const itemsNull = [1,2,3,4,5].map((value) => {
+  return itemNull(value)
+});
+
+const propsItemsNull = {
+  items: itemsNull,
+  department: 2,
+  initialSelectedNational,
+  initialSelectedProvincial
+}
+
+
+// case where provinces list is an empty object
+const itemEmpty = (value) => (
+  provincesEmpty(value)
+);
+
+const itemsEmpty = [1,2,3,4,5].map((value) => {
+  return itemEmpty(value)
+});
+
+const propsitemsEmpty = {
+  items: itemsEmpty,
+  department: 2,
+  initialSelectedNational,
+  initialSelectedProvincial
+}
+
+
+//case where every equitable share is excluded from National Treemap footer
+const itemNoEquitableShare = (value) => (
+  noEquitableShare(value)
+);
+
+const itemsNoEquitableShare = [1,2,3,4,5].map((value) => {
+  return itemNoEquitableShare(value)
+});
+
+const propsNoEquitableShare = {
+  items: itemsNoEquitableShare,
+  department: 2,
+  initialSelectedNational,
+  initialSelectedProvincial
 }
 
 
 const basic = () => <FocusAreaTemp {...props} />;
+const NoInitialSelectedProvincial = () => <FocusAreaTemp {...propsNoInitialSelectedProvincial} />;
+const ItemsNull = () => <FocusAreaTemp {...propsItemsNull} />;
+const ItemsEmpty = () => <FocusAreaTemp {...propsitemsEmpty} />;
+const NoEquitableShareFooter = () => <FocusAreaTemp {...propsNoEquitableShare} />;
 
 
 storiesOf('view.FocusAreaTemp', module)
   .add('Default', basic)
+  .add('NoInitialSelectedProvincial', NoInitialSelectedProvincial)
+  .add('ItemsNull', ItemsNull)
+  .add('ItemsEmpty', ItemsEmpty)
+  .add('NoEquitableShareFooter', NoEquitableShareFooter)
