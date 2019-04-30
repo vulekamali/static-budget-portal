15176234fde4e145ca0eada12d647dde87fd96f9
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-30 16:33:37 +0200

    remove redundant comment for total and percentage

--- packages/webapp/src/adaptors/focus-areas-preview/addProvinceToObject.js
+++ packages/webapp/src/adaptors/focus-areas-preview/addProvinceToObject.js
@@ -1,28 +1,28 @@
 const addProvinceToObject = (provincialData) => {
-  return (['Eastern Cape','Free State','Gauteng','Limpopo','Mpumalanga','Northern Cape','Western Cape','North West', 'KwaZulu-Natal'].reduce(
-    (result, provinceName) => {
-      const children = provincialData.filter(item => item.province === provinceName).map(({ slug, percentage_of_total, ...data }) => ({
-        ...data,
-        id: slug,
-        percentage: percentage_of_total,
-      }));
-    
-      const amount = children.reduce((result, { amount }) => result + amount, 0);
-      // const percentage = (amount / total) * 100;
+  return (
+    ['Eastern Cape','Free State','Gauteng','Limpopo','Mpumalanga','Northern Cape','Western Cape','North West', 'KwaZulu-Natal'].reduce(
+      (result, provinceName) => {
+        const children = provincialData.filter(item => item.province === provinceName).map(({ slug, percentage_of_total, ...data }) => ({
+          ...data,
+          id: slug,
+          percentage: percentage_of_total,
+        }));
       
-      return {
-        ...result,
-        [provinceName]: {
-          name: provinceName,
-          amount,
-          // percentage,
-          children
-        }
-      };
-    },
-    {},
+        const amount = children.reduce((result, { amount }) => result + amount, 0);
+        
+        return {
+          ...result,
+          [provinceName]: {
+            name: provinceName,
+            amount,
+            children
+          }
+        };
+      },
+      {},
+    )
   )
-)}
+}
 
 
 export default addProvinceToObject;
