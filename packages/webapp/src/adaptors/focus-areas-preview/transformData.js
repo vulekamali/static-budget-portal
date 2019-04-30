6509d433897823730ebe3e41d5b9abe1c0bf4e65
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-04-30 13:24:23 +0200

    adjust transform data for provinces

--- packages/webapp/src/adaptors/focus-areas-preview/transformData.js
+++ packages/webapp/src/adaptors/focus-areas-preview/transformData.js
@@ -1,30 +1,4 @@
-const createChildren = ({ amount, name, province, slug, percentage_of_total }) => ({
-  amount,
-  name,
-  province,
-  id: slug,
-  percentage: percentage_of_total
-});
-
-const createProvinces = details => {
-  const { province, name, slug, percentage_of_total, amount, url } = details;
-  return {
-    [province]: {
-      name,
-      amount,
-      id: slug,
-      url,
-      percentage: percentage_of_total,
-      children: createChildren(details)
-    }
-  }
-};
-
-// const createProvincialObjects = ({ notices, footnotes, data }) => ({
-//   notices,
-//   footnotes,
-//   provinces: data.map(createProvinces)
-// });
+import addProvinceToObject from './addProvinceToObject';
 
 const createNationalDepartments = ({ title, slug, amount }) => ({
   name: title,
@@ -32,14 +6,8 @@ const createNationalDepartments = ({ title, slug, amount }) => ({
   url: slug
 });
 
-// const createNationalObjects = ({ notices, footnotes, data }) => ({
-//   notices,
-//   footnotes,
-//   departments: data.map(createNationalDepartments)
-// });
 
 const transformData = (response) => {
-  console.log(response)
   const focusSchema = response.items.map(department => {
     const {
       total,
@@ -50,6 +18,7 @@ const transformData = (response) => {
     } = department;
 
     const { notices: nationalNotices, footnotes: nationalFootnotes, data: nationalData } = national;
+
     const { notices: provincialNotices, footnotes: provincialFootnotes, data: provincialData } = provincial;
 
     return {
@@ -64,21 +33,13 @@ const transformData = (response) => {
       provincial: {
         notices: provincialNotices,
         footnotes: provincialFootnotes,
-        provinces: provincialData.map(createProvinces)
+        provinces: addProvinceToObject(provincialData)
       }
     }
   })
-
-  console.log(focusSchema)
 
  return focusSchema;
 
 };
 
 export default transformData;
-
-
-
-// notices: nationalNotices,
-//         footnotes: nationalFootnotes,
-//         departments: nationalData.map(createNationalDepartments) 
