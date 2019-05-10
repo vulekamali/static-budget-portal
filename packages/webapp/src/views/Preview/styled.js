c2bf029e1525c1c9846423393df0cbe40dc731e2
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-10 16:52:44 +0200

    style focus links section

--- packages/webapp/src/views/Preview/styled.js
+++ packages/webapp/src/views/Preview/styled.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-import { Typography } from '@material-ui/core';
+import { Typography, Button } from '@material-ui/core';
 
 const Wrapper = styled.div`
   background: #fff;
@@ -92,6 +92,62 @@ const FooterDetails = styled(Typography)`
   }
 `;
 
+const FocusWrapper = styled.div`
+  margin-top: 48px;
+`;
+
+const FocusLinksWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-right: 16px;
+  margin-left: 16px;
+
+  @media screen and (min-width: 550px) {
+    margin-right: 48px;
+    margin-left: 48px;
+  }
+`;
+
+const FocusLinksContainer = styled.div`
+  margin-top: 8px;
+  width: 100%;
+  max-width: 1200px;
+
+  @media screen and (min-width: 600px) {
+    margin-bottom: 16px;
+    display: flex;
+    flex-wrap: wrap;
+  }
+`;
+
+const ButtonContainer = styled.div`
+  margin-bottom: 16px;
+  margin-right: 16px;
+`;
+
+const Link = styled.a`
+  text-decoration: none;
+`;
+
+const ButtonStyled = styled(Button)`
+  && {
+    background: rgba(0, 0, 0, 0.1);
+    border-radius: 2px;
+    padding: 16px 36px;
+  }
+`;
+
+const TextButton = styled(Typography)`
+  && {
+    color: #000;
+    font-size: 20px;
+    font-weight: 700;
+    line-height: 24px;
+    letter-spacing: 0.15px;
+  }
+`;
+
 export {
   Wrapper,
   TextWrapper,
@@ -99,7 +155,14 @@ export {
   Description,
   FooterWrapper,
   FooterContainer,
-  FooterDetails
+  FooterDetails,
+  FocusWrapper,
+  Link,
+  ButtonStyled,
+  TextButton,
+  FocusLinksWrapper,
+  FocusLinksContainer,
+  ButtonContainer,
 }
 
 export default {
@@ -109,5 +172,12 @@ export default {
   Description,
   FooterWrapper,           
   FooterContainer,
-  FooterDetails
+  FooterDetails,
+  FocusWrapper,
+  Link,
+  ButtonStyled,
+  TextButton,
+  FocusLinksWrapper,
+  FocusLinksContainer,
+  ButtonContainer,
 }
