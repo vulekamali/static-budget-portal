235ccb835c153bf5fbdf388cc65dbfafb0e85be6
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-15 12:22:39 +0200

    fix prettier linting

--- packages/webapp/src/components/SectionHeading/index.story.jsx
+++ packages/webapp/src/components/SectionHeading/index.story.jsx
@@ -3,12 +3,21 @@ import { storiesOf } from '@storybook/react';
 
 import SectionHeading from './index';
 
-const share = () => <SectionHeading title='Provincial Budget Summary' share>Children Components</SectionHeading>;
-const noShare = () => <SectionHeading title='Provincial Budget Summary'>Children Components</SectionHeading>;
-const shareString = () => <SectionHeading title='Provincial Budget Summary' share='addTextHere'>Children Components</SectionHeading>;
-
+const share = () => (
+  <SectionHeading title="Provincial Budget Summary" share>
+    Children Components
+  </SectionHeading>
+);
+const noShare = () => (
+  <SectionHeading title="Provincial Budget Summary">Children Components</SectionHeading>
+);
+const shareString = () => (
+  <SectionHeading title="Provincial Budget Summary" share="addTextHere">
+    Children Components
+  </SectionHeading>
+);
 
 storiesOf('components.SectionHeading', module)
   .add('Share', share)
   .add('No Share', noShare)
-  .add('Share is a String', shareString)
+  .add('Share is a String', shareString);
