8d9c7a622327f35bd9a2f80f525fd9e42990b5b7
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-27 16:48:12 +0200

    remove redundant stories

--- packages/webapp/src/components/FilterDropdown/__test__/index.stories.tsx
+++ packages/webapp/src/components/FilterDropdown/__test__/index.stories.tsx
@@ -3,14 +3,6 @@ import { storiesOf } from '@storybook/react';
 
 import FilterDropdown from '../';
 
-const phases = {
-  disabled: 'Original budget',
-};
-
-const years = {
-  disabled: '2018-19',
-};
-
 const options = [
   {
     id: 'random1',
@@ -29,8 +21,6 @@ const options = [
   },
 ];
 
-const phasesDropdown = () => <FilterDropdown {...{ phases }} />;
-const yearsDropdown = () => <FilterDropdown {...{ years }} />;
 const optionsDropdownPrimary = () => <FilterDropdown {...{ options }} />;
 const optionsDropdownSecondary = () => <FilterDropdown {...{ options }} greyTheme />;
 const PrimaryDisabled = () => <FilterDropdown {...{ options }} disabled />;
@@ -39,8 +29,6 @@ const PrimaryLoading = () => <FilterDropdown {...{ options }} loading />;
 const SecondaryLoading = () => <FilterDropdown {...{ options }} greyTheme loading />;
 
 storiesOf('components.FilterDropdown', module)
-  .add('Phases', phasesDropdown)
-  .add('Years', yearsDropdown)
   .add('Options Primary', optionsDropdownPrimary)
   .add('Options Secondary', optionsDropdownSecondary)
   .add('Primary Disabled', PrimaryDisabled)
