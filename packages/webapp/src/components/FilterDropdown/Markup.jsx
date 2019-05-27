72cf954c4064ca06c5fd1b2b6677a78fc46597c3
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-27 16:21:26 +0200

    start adding loading state

--- packages/webapp/src/components/FilterDropdown/Markup.jsx
+++ packages/webapp/src/components/FilterDropdown/Markup.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import { MenuItem, CssBaseline } from '@material-ui/core';
 
-import { FormContainer, SelectPreview } from './styled';
+import { FormContainer, SelectPreview, CircularProgressStyled } from './styled';
 
-const callMenuItems = options =>
-  options.map(({ id: idVal, name }) => {
+const callSpinner = () => <CircularProgressStyled size={20} thickness={2.5} />;
+
+const callMenuItems = (options, loading) => {
+  if (loading) {
+    return <MenuItem>{callSpinner()}</MenuItem>;
+  }
+
+  return options.map(({ id: idVal, name }) => {
     const selectedKey = options.findIndex(({ id }) => id === idVal);
 
     return (
@@ -13,8 +19,9 @@ const callMenuItems = options =>
       </MenuItem>
     );
   });
+};
 
-const callOptions = (options, selected, eventHandler, greyTheme, disabled) => (
+const callOptions = (options, selected, eventHandler, greyTheme, disabled, loading) => (
   <SelectPreview
     {...{ greyTheme }}
     disabled={disabled}
@@ -24,14 +31,14 @@ const callOptions = (options, selected, eventHandler, greyTheme, disabled) => (
     name={selected}
     classes={{ icon: 'icon', selectMenu: 'selectMenu' }}
   >
-    {callMenuItems(options)}
+    {callMenuItems(options, loading)}
   </SelectPreview>
 );
 
-const FilterDropdown = ({ options, selected, eventHandler, greyTheme, disabled }) => (
+const FilterDropdown = ({ options, selected, eventHandler, greyTheme, disabled, loading }) => (
   <FormContainer>
     <CssBaseline />
-    {options && callOptions(options, selected, eventHandler, greyTheme, disabled)}
+    {options && callOptions(options, selected, eventHandler, greyTheme, disabled, loading)}
   </FormContainer>
 );
 
