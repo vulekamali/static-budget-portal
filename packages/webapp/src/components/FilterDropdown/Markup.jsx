f04eef088bdc99eaa375453eef6e302a80218afc
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-27 15:49:21 +0200

    refactor

--- packages/webapp/src/components/FilterDropdown/Markup.jsx
+++ packages/webapp/src/components/FilterDropdown/Markup.jsx
@@ -1,31 +1,7 @@
 import React from 'react';
 import { MenuItem, CssBaseline } from '@material-ui/core';
 
-import { FormContainer, SelectStyledPhase, SelectPreview } from './styled';
-
-const callBudgetPhaseSelect = phases => (
-  <SelectPreview
-    value={phases.disabled}
-    disabled
-    displayEmpty
-    classes={{ selectMenu: 'selectMenu', disabled: 'disabled', icon: 'icon' }}
-    greyTheme
-  >
-    <MenuItem value={phases.disabled}>{phases.disabled}</MenuItem>
-  </SelectPreview>
-);
-
-const callYearsSelect = years => (
-  <SelectPreview
-    value={years.disabled}
-    disabled
-    displayEmpty
-    classes={{ selectMenu: 'selectMenu', disabled: 'disabled', icon: 'icon' }}
-    greyTheme
-  >
-    <MenuItem value={years.disabled}>{years.disabled}</MenuItem>
-  </SelectPreview>
-);
+import { FormContainer, SelectPreview } from './styled';
 
 const callMenuItems = options =>
   options.map(({ id: idVal, name }) => {
@@ -52,19 +28,9 @@ const callOptions = (options, selected, eventHandler, greyTheme, disabled) => (
   </SelectPreview>
 );
 
-const FilterDropdown = ({
-  phases,
-  years,
-  options,
-  selected,
-  eventHandler,
-  greyTheme,
-  disabled,
-}) => (
+const FilterDropdown = ({ options, selected, eventHandler, greyTheme, disabled }) => (
   <FormContainer>
     <CssBaseline />
-    {phases && callBudgetPhaseSelect(phases)}
-    {years && callYearsSelect(years)}
     {options && callOptions(options, selected, eventHandler, greyTheme, disabled)}
   </FormContainer>
 );
