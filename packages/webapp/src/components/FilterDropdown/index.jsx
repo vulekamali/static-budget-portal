bebfa63cff8b04bf697b7ec7b0213adfa892cbab
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-28 16:37:46 +0200

    remove state

--- packages/webapp/src/components/FilterDropdown/index.jsx
+++ packages/webapp/src/components/FilterDropdown/index.jsx
@@ -1,42 +1,41 @@
-import React, { Component } from 'react';
-import Markup from './Markup';
+import React from 'react';
+import { MenuItem } from '@material-ui/core';
 
-class FilterDropdown extends Component {
-  constructor(props) {
-    super(props);
-    this.onSelectionChange = this.onSelectionChange.bind(this);
+import { SelectPreview, SpinnerContainer, CircularProgressStyled } from './styled';
 
-    this.state = {
-      selected: '',
-      loading: false,
-    };
+const callSpinner = () => <CircularProgressStyled size={20} thickness={2.5} />;
 
-    this.events = {
-      onSelectionChange: this.onSelectionChange.bind(this),
-    };
+const callMenuItems = (options, loading) => {
+  if (loading) {
+    return (
+      <MenuItem value={options[0].id}>
+        <SpinnerContainer>{callSpinner()}</SpinnerContainer>
+      </MenuItem>
+    );
   }
 
-  onSelectionChange(e) {
-    const { selected } = this.state;
-    if (e.target.value === selected) {
-      return null;
-    }
-    this.setState({ selected: e.target.value });
-    return null;
-  }
+  return options.map(({ id, name }) => (
+    <MenuItem key={id} value={id}>
+      {name}
+    </MenuItem>
+  ));
+};
 
-  render() {
-    const { state, events, props } = this;
+const callOptions = (options, selected, onSelectionChange, primary, loading) => (
+  <SelectPreview
+    {...{ primary }}
+    disabled={options.length <= 1 || loading}
+    value={selected || options[0].id}
+    onChange={onSelectionChange}
+    displayEmpty
+    name={selected}
+    classes={{ icon: 'icon', selectMenu: 'selectMenu', disabled: 'disabled' }}
+  >
+    {options.length > 0 && callMenuItems(options, loading)}
+  </SelectPreview>
+);
 
-    const passedProps = {
-      ...props,
-      onSelectionChange: events.onSelectionChange,
-      selected: state.selected,
-      loading: state.loading,
-    };
-
-    return <Markup {...passedProps} />;
-  }
-}
+const FilterDropdown = ({ options, selected, onSelectionChange, primary, loading }) =>
+  callOptions(options, selected, onSelectionChange, primary, loading);
 
 export default FilterDropdown;
