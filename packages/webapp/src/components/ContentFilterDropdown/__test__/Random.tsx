5b4f64225caca1808848caa3e3d69eb6fd708d94
Zeeshaan <maudarbocus.zeeshaan@gmail.com>
2019-05-30 10:28:36 +0200

    delete redundant files

--- packages/webapp/src/components/ContentFilterDropdown/__test__/Random.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { mockPresentationprops, TpresentationProps } from '../schema';
-import ContentFilterDropdown from '../index';
-
-const passedProps: TpresentationProps = mockPresentationprops();
-
-const Test = () => <ContentFilterDropdown {...passedProps} />;
-
-export default Test;
