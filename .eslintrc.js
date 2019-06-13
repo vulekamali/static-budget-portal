8b8d47dc3b2f0bfd0af8a4702daccb16c7f83162
Schalk Venter <venterschalk@gmail.com>
2019-06-13 08:39:10 +0200

    Updates enivornment configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,53 +1,57 @@
 module.exports = {
-  "parser": "babel-eslint",
-  "env": {
-    "browser": true,
-    "es6": true,
-    "jest": true,
-    "node": true
-  },
-  parserOptions:  {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'prettier',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:jest/recommended',
+    'plugin:sonarjs/recommended',
+    'plugin:lodash/recommended',
+  ],
+  parserOptions: {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  'plugins': [
+  plugins: [
     'react',
     'react-app',
     'prettier',
     'jest',
     'compat',
-    'extra-rules',
+    'filenames',
+    'sonarjs',
+    'optimize-regex',
+    'jsdoc',
+    'lodash',
   ],
-  "settings": {
-    "polyfills": [
-      "fetch",
-      "promises"
-    ]
-  },
-  extends: [
-    'airbnb',
-    'prettier',
-    'plugin:prettier/recommended',
-  ],
-  "rules": {
-    "react/prop-types": "off",
-    "linebreak-style": "off",
-  },
-  'env': {
+  env: {
     'jest/globals': true,
     browser: true,
     node: true,
   },
-  'rules': {
+  rules: {
     'prettier/prettier': ['error'],
     'linebreak-style': 'off',
     'react/prop-types': 'off',
-    'jsx-a11y/href-no-hash': 'off',
-    'react/jsx-one-expression-per-line': 'off' // Conflicts with Prettier
+    'react/jsx-filename-extension': ['error', { extensions: ['.jsx', '.tsx'] }],
+    'react/jsx-one-expression-per-line': 'off', // Conflicts with Prettier
+    '@typescript-eslint/prefer-interface': 'off',
+    'filenames/match-exported': 'warn',
+    'optimize-regex/optimize-regex': 'warn',
+    'jsdoc/require-jsdoc': 'error',
+    'lodash/prefer-lodash-method': 'off',
+    'lodash/prefer-constant': 'off',
+    'no-warning-comments': ["error", { "terms": ["todo", "fixme"], "location": "anywhere" }],
+    'jest/valid-describe': 'off',
   },
   settings: {
-    react:  {
-      version:  'detect',
+    'import/resolver': {
+      typescript: {},
+    },
+    react: {
+      version: 'detect',
     },
   },
-}
+};
