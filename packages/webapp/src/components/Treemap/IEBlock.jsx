4bab741df2490e38ffbe79ba2c013f9fa2298cff
JD Bothma <jbothma@gmail.com>
2019-06-25 23:36:06 +0200

    Add truncated labels

--- packages/webapp/src/components/Treemap/IEBlock.jsx
+++ packages/webapp/src/components/Treemap/IEBlock.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
 import { lighten } from 'polished';
+import trimValues from '../../helpers/trimValues';
+
+
+function makeLabel(x, y, width, height, text, isBold) {
+  var fontSize = 14;
+  var padding = 10;
+  var estimatedWidth = text.length * (fontSize*0.6);
+  if (estimatedWidth > width) {
+    var estimatedFittingChars = Math.floor(width / fontSize);
+    if (estimatedFittingChars < 4)
+      return null;
+
+    console.log("truncated", {width, text, estimatedWidth, chars: text.length, estimatedFittingChars});
+    text = text.slice(0, estimatedFittingChars) + "...";
+  } else {
+    console.log("fine", {width, text, estimatedWidth});
+  }
+  const style = {fontSize, fontFamily: "Roboto, sans-serif", fontWeight: isBold ? "600" : '400'};
+  return <text x={x + padding} y={y + fontSize + padding} style={style}>{text}</text>;
+}
+
 
 const IEBlock = props => {
   const {
@@ -21,8 +42,6 @@ const IEBlock = props => {
     icon,
   } = props;
 
-  if (depth < 1) return null;
-
   if (depth === 1) {
     return (
       <g>
@@ -35,6 +54,8 @@ const IEBlock = props => {
             fill: color,
           }}
         />
+        {makeLabel(x, y, width, height, name, true)}
+        {makeLabel(x, y+19, width, height, `R${trimValues(amount, true)}`, false)}
       </g>
     );
   }
@@ -56,6 +77,10 @@ const IEBlock = props => {
       </g>
     );
   }
+
+  // depth < 1
+  return null;
 };
+
 
 export default IEBlock;
